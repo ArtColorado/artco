444abfa0ea89935fa47fc5edb264b1d4b4a540a0
ItsToastCrumb <160540794+ItsToastCrumb@users.noreply.github.com>
2024-05-07 12:52:46 -0600

    merging base directory and boilerplate

---
+++ client/src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+import App from "./App.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
