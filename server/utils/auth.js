4a337672933e39b16c0fe3cb5493a6a8d8c9d00f
TME <tyler.s.meyers@gmail.com>
2024-05-07 16:20:16 -0600

    auth with jwt

---
+++ server/utils/auth.js
@@ -0,0 +1,19 @@
+const { GraphQLError } = require("graphql");
+const jwt = require("jsonwebtoken");
+
+const secret = process.env.SECRET;
+const expiration = "8h";
+
+module.exports = {
+  AuthenticationError: new GraphQLError("Could not authenticate user.", {
+    extensions: {
+      code: "UNAUTHENTICATED",
+    },
+  }),
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, name, _id };
+    return jwt.sign({ data: payload }, secret, {
+      expiresIn: expiration,
+    });
+  },
+};
