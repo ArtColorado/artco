243d2a3269195441f21ee8f645533903d8e7ff53
BookWyrmMom <160056156+BookWyrmMom@users.noreply.github.com>
2024-05-08 14:04:42 -0600

    progress on header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -15,52 +15,50 @@ const Header = () => {
   const handleShow = () => setShow(true);
 
   return (
-    <div className="header">
-      <Navbar bg="dark" sticky="top">
-        <Container>
-          <h1 className="text-bg-dark text-center">artCO</h1>
-          <Button variant="success" onClick={handleShow}>
-            <span className="navbar-toggler-icon"></span>
-          </Button>
-          <Offcanvas show={show} onHide={handleClose} placement="end">
-            <Offcanvas.Header closeButton>
-              <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
-                Navigation
-              </h5>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <Nav.Item>
-                  <Nav.Link href="/">Home</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="/event">Events</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="/artist">Artists</Nav.Link>
-                </Nav.Item>
-                {/* {Auth.loggedIn() ? ( */}
-                {/* <> */}
-                <Nav.Item>
-                  <Nav.Link href="/me">My Profile</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="#" id="logout">
-                    Logout
-                  </Nav.Link>
-                </Nav.Item>
-                {/* </> */}
-                {/* ) : ( */}
-                <Nav.Item>
-                  <Nav.Link href="/login">Login/Signup</Nav.Link>
-                </Nav.Item>
-                {/* )} */}
-              </Nav>
-            </Offcanvas.Body>
-          </Offcanvas>
-        </Container>
-      </Navbar>
-    </div>
+    <Navbar sticky="top">
+      <Container>
+        <h1 className="text-center">artCO</h1>
+        <Button variant="success" onClick={handleShow}>
+          <span className="navbar-toggler-icon"></span>
+        </Button>
+        <Offcanvas show={show} onHide={handleClose} placement="end">
+          <Offcanvas.Header closeButton>
+            <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
+              Navigation
+            </h5>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <Nav.Item>
+                <Nav.Link href="/">Home</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/event">Events</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/artist">Artists</Nav.Link>
+              </Nav.Item>
+              {/* {Auth.loggedIn() ? ( */}
+              {/* <> */}
+              <Nav.Item>
+                <Nav.Link href="/me">My Profile</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#" id="logout">
+                  Logout
+                </Nav.Link>
+              </Nav.Item>
+              {/* </> */}
+              {/* ) : ( */}
+              <Nav.Item>
+                <Nav.Link href="/login">Login/Signup</Nav.Link>
+              </Nav.Item>
+              {/* )} */}
+            </Nav>
+          </Offcanvas.Body>
+        </Offcanvas>
+      </Container>
+    </Navbar>
   );
 };
 
