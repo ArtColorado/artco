38b86527336033d7ce59f3a654266a60bc6e65d1
mikededo58 <159196932+mikededo58@users.noreply.github.com>
2024-05-08 18:45:15 -0600

    updated schemas and added quieries on client side

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,53 @@
 import { gql } from "@apollo/client";
+
+export const QUERY_ARTIST = gql`
+  query User($userId: ID!) {
+    user(userId: $userId) {
+      email
+      is_artist
+      artistData {
+        events {
+          location
+          date
+          name
+        }
+        bio
+        artWorks {
+          _id
+          description
+          imageURL
+          stock
+          title
+        }
+      }
+      address
+    }
+  }
+`;
+
+export const QUERY_ARTISTS = gql`
+  query Users {
+    users {
+      is_artist
+      artistData {
+        events {
+          date
+          location
+          name
+        }
+      }
+    }
+  }
+`;
+
+export const QUERY_ARTWORK = gql`
+  query Artwork($artworkId: ID!) {
+    artwork(artworkId: $artworkId) {
+      _id
+      description
+      imageURL
+      stock
+      title
+    }
+  }
+`;
