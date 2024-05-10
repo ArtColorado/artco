94c0a8dfeeafc33a35ceda0ff6d46e9cd8e6d14c
mikededo58 <159196932+mikededo58@users.noreply.github.com>
2024-05-10 09:59:23 -0600

    completed auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,9 +14,23 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ThemeProvider from "./utils/themeContext";
 
-// const httpLink = createHttpLink({
-//   uri: "/graphql",
-// });
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("authToken");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const apolloClient = new ApolloClient({ cache: new InMemoryCache(), link: authLink.concat(httpLink) });
 
 function App() {
   const [count, setCount] = useState(0);
